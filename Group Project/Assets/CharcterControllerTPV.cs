bcab01ec0a03e49a341a03fc94ca634963769f64
Rayyan-Gafoor <60914125+Rayyan-Gafoor@users.noreply.github.com>
2021-08-27 17:14:48 +0200

    Gravity function

--- Group Project/Assets/CharcterControllerTPV.cs
+++ Group Project/Assets/CharcterControllerTPV.cs
@@ -10,6 +10,14 @@ public class CharcterControllerTPV : MonoBehaviour
     [Header("Movement")]
     public float Speed;
     public float Jump;
+    Vector3 move;
+   [Header("Gravity")]
+    public float Gravity;
+    public float FallVelocity;
+    [SerializeField] Transform GroundCheck;
+    [SerializeField] LayerMask Ground;
+    [SerializeField] bool isGrounded;
+    //[SerializeField] Vector3 velocity;
 
     [Header("Camera Input")]
     public Transform Camera;
@@ -30,15 +38,24 @@ public class CharcterControllerTPV : MonoBehaviour
         //Movement
         float HoriInput = Input.GetAxisRaw("Horizontal");
         float VertiInput = Input.GetAxisRaw("Vertical");
-
+        //Gravity -= 9.81f * Time.deltaTime;
         Vector3 Direction = new Vector3(HoriInput, 0f, VertiInput).normalized;
-
-        if(Direction.magnitude>= 0.1f)
+        //Gravity
+        GravityForce();
+        if (Physics.CheckSphere(GroundCheck.position, 0.1f, Ground))
+        {
+            isGrounded = true;
+        }
+        else
+        {
+            isGrounded = false;
+        }
+        if (Direction.magnitude>= 0.1f)
         {
             float Angle = Mathf.Atan2(Direction.x, Direction.z) * Mathf.Rad2Deg + Camera.eulerAngles.y;
             float angle_ = Mathf.SmoothDampAngle(transform.eulerAngles.y, Angle, ref Torque, CamLerp);
             transform.rotation = Quaternion.Euler(0f, angle_, 0f);
-            Vector3 move = Quaternion.Euler(0f, Angle, 0f) * Vector3.forward;
+            move = Quaternion.Euler(0f, Angle, 0f) * Vector3.forward;
             controller.Move(move.normalized * Speed * Time.deltaTime);
         }
         // Jumping 
@@ -50,5 +67,19 @@ public class CharcterControllerTPV : MonoBehaviour
             }
 
         }*/
+    }
+    void GravityForce()
+    {
+        if (!isGrounded)
+        {
+            // move += Physics.gravity;
+           Vector3 Grav = new Vector3(0f, Gravity, 0f);
+           controller.Move(Grav* FallVelocity* Time.deltaTime);
+        }
+        else
+        {
+            return;
+        }
+
     }
 }
